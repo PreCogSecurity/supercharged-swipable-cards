7b1ae1f4fad36b0262df15b57b70a342ba3cc3cf
Eric Sayler <easayler@gmail.com>
2017-02-13 14:38:51 -0700

    set up webpack, eslint, finish iteration 1

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'cheap-eval-source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+    ],
+  },
+}
